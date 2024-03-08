d0778b1b84fb250ac14cd71f74e8e78130caeb96
gpaipalondica <gpaipalondica@sycamores.indstate.edu>
2024-03-08 16:04:21 -0600

    sound

--- src/Components/Chords.js
+++ src/Components/Chords.js
@@ -77,6 +77,19 @@ function Chords() {
     document.querySelector('.searchresult').style.display='none';
   }
 
+  function playSound(x,y){
+    console.log(x,y);
+    let whichChord = Chordlist.find(item => item.name === x)
+    if(whichChord){
+      if(y==='guitar'){
+        new Audio(whichChord.gsound).play();
+      }else if(y==='piano'){
+        new Audio(whichChord.psound).play();
+      }
+    }
+    
+  }
+
 
   return (
     <div className='chords'>
@@ -120,7 +133,15 @@ function Chords() {
         .map((x,i) => {
           return(
           <div key={i} className="grid-item">
-            <p className="chord-name">{x.name}</p>
+            <div className='grid-top'>
+              <p className="chord-name">{x.name}</p>
+              { ((cat  === 'guitar' &&  x.gsound) ||  (cat === 'piano' && x.psound))
+               ?
+              <button onClick={() => playSound(x.name,cat)}><svg stroke="currentColor" fill="currentColor" strokeWidth="0" height="1em" width="1em" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12 8.02c0 1.09-.45 2.09-1.17 2.83l-.67-.67c.55-.56.89-1.31.89-2.16 0-.85-.34-1.61-.89-2.16l.67-.67A3.99 3.99 0 0 1 12 8.02zM7.72 2.28L4 6H2c-.55 0-1 .45-1 1v2c0 .55.45 1 1 1h2l3.72 3.72c.47.47 1.28.14 1.28-.53V2.81c0-.67-.81-1-1.28-.53zm5.94.08l-.67.67a6.996 6.996 0 0 1 2.06 4.98c0 1.94-.78 3.7-2.06 4.98l.67.67A7.973 7.973 0 0 0 16 8c0-2.22-.89-4.22-2.34-5.66v.02zm-1.41 1.41l-.69.67a5.05 5.05 0 0 1 1.48 3.58c0 1.39-.56 2.66-1.48 3.56l.69.67A5.971 5.971 0 0 0 14 8.02c0-1.65-.67-3.16-1.75-4.25z"></path></svg></button>
+              :  
+              <button disabled><svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" stroke="#000" strokeWidth="2" d="M1,8 L1,16 L6.09901951,16 L12,21 L12,3 L6,8 L1,8 Z M15,9 L21,15 M21,9 L15,15"></path></svg></button>
+            }
+            </div>
             <img className='chord-image' src={x[cat]} alt="" />
           </div>
           )
